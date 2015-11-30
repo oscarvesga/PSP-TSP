980679833559d5c0fc6de7be3f382dd07df82266
Oscar Vesga <oscarvesga@gmail.com>
2015-11-30 15:16:16 -0500

    CODE Programa 7

--- Programa_7_code/src/kernel/Calculadora.java
+++ Programa_7_code/src/kernel/Calculadora.java
@@ -6,17 +6,23 @@ package kernel;
  *
  * @author Oscar Vesga
  * @version 1.0
- * @since 01-09-2015
+ * @since 23-11-2015
  *
  */
 public class Calculadora
 {
+
+    private final int num_seg;
+
+    private final double error;
 
     /**
      * Constructor
      */
     public Calculadora()
     {
+        this.num_seg = 10;
+        this.error = 0.0000001;
     }
 
     /**
@@ -243,5 +249,223 @@ public class Calculadora
         float B1 = calcularB1(data);
 
         return B0 + B1 * proxyE;
+    }
+
+    /**
+     * Calcula la significancia de los datos
+     *
+     * @param data
+     * @param proxyE
+     * @return
+     */
+    double calcularSignificancia(Head data)
+    {
+        double r = this.calcularR(data);
+        double r2 = r * r;
+        int n = data.size();
+
+        double t = (r * Math.sqrt(n - 2)) / Math.sqrt(1 - r2);
+        double p = this.findProbP(t, n - 2);
+
+        return 1 - (2 * p);
+    }
+
+    /**
+     * Calcula el valor P teniendo en cuenta el error E
+     *
+     * @param data
+     * @param t
+     * @return
+     */
+    private double findProbP(double t, int dof)
+    {
+        double p1 = funcionP(t, dof, this.num_seg);
+        double p2 = funcionP(t, dof, 2 * this.num_seg);
+
+        int j = 2 * this.num_seg;
+        while (p2 - p1 >= this.error) {
+            p1 = funcionP(t, dof, j);
+            p2 = funcionP(t, dof, j * 2);
+            j = j * 2;
+        }
+
+        return p2;
+    }
+
+    /**
+     * Calcula la funcion P
+     *
+     * @param x
+     * @param dof grados de libertad
+     * @param num_seg numero de segmentos de la función
+     * @return
+     */
+    public double funcionP(double x, int dof, int num_seg)
+    {
+        double result = 0;
+        double w = x / num_seg;
+
+        for (int i = 1; i < num_seg; i++) {
+            if (i % 2 == 0) {
+                result += 2 * funcionF(w * i, dof);
+            } else {
+                result += 4 * funcionF(w * i, dof);
+            }
+        }
+
+        double suma = funcionF(0, dof) + result + funcionF(x, dof);
+        return suma * w / 3;
+    }
+
+    /**
+     * Calcula la funcion F
+     *
+     * @param xi segmento a caluclar
+     * @param dof grados de libertad
+     * @return
+     */
+    public double funcionF(double xi, int dof)
+    {
+        double result1 = funcionGamma((dof + 1) / 2f);
+        result1 = result1 / (Math.pow(dof * Math.PI, 1 / 2f) * funcionGamma(dof / 2f));
+        double result2 = 1 + (xi * xi / dof);
+        result2 = Math.pow(result2, -(dof + 1) / 2f);
+
+        return result1 * result2;
+    }
+
+    /**
+     * Calcula el coeficiente Gamma
+     *
+     * @param xi
+     * @return el coeficiente Gamma
+     */
+    public double funcionGamma(float xi)
+    {
+        if (xi == 1) {
+            return 1;
+        } else if (xi == 0.5) {
+            return Math.sqrt(Math.PI);
+        } else {
+            return (xi - 1) * funcionGamma(xi - 1);
+        }
+    }
+
+    /**
+     * Calcula el rango
+     *
+     * @param data
+     * @param proxyE
+     * @return
+     */
+    double calcularRange(Head data, float proxyE)
+    {
+        int n = data.size();
+        float Xavg = this.getXavg(data);
+
+        Node node = data.getFirst();
+        double X_Xavg2 = 0;
+        while (node != null) {
+            X_Xavg2 += Math.pow(node.getX() - Xavg, 2);
+            node = node.getNext();
+        }
+
+        double desvEstandar = this.calcularDesvEstandar(data);
+        double t = this.calcularT(0.35f, n - 2);
+        double rest = 1 + (1f / n) + (Math.pow(proxyE - Xavg, 2) / (X_Xavg2));
+
+        return t * desvEstandar * Math.sqrt(rest);
+    }
+
+    /**
+     * Calcula la desviacion estandar de un grupo de datos
+     *
+     * @param data grupo de datos
+     * @return
+     */
+    private double calcularDesvEstandar(Head data)
+    {
+        int n = data.size();
+        float B0 = this.calcularB0(data);
+        float B1 = this.calcularB1(data);
+
+        double rest = 0;
+        Node node = data.getFirst();
+        while (node != null) {
+            float x = node.getX();
+            float y = node.getY();
+            float sum = y - B0;
+            sum = sum - (B1 * x);
+            rest += Math.pow(sum, 2);
+
+            node = node.getNext();
+        }
+
+        n = n - 2;
+        return Math.sqrt(rest * (1f / n));
+    }
+
+    /**
+     * Calcula el valor T teniendo en cuenta el error E
+     *
+     * @param data datos para calcular el valor T
+     * @return retorna una lista de valores T asociados a la lista de datos data
+     */
+    private double calcularT(double p, int dof)
+    {
+        double t = 1.0d;
+        double d = 0.5d;
+
+        double p1 = 0.0d;
+        double p2 = 0.5d;
+        double err = 0.0d;
+        do {
+            p1 = p;
+            p2 = funcionP(t, dof, this.num_seg);
+            if (p1 - p2 > 0) {
+                t = t + d;
+            } else {
+                d = d / 2;
+                t = t - d;
+            }
+
+            p2 = funcionP(t, dof, this.num_seg);
+            err = p1 - p2;
+            if (err < 0) {
+                err = err * -1;
+            }
+        } while (err >= this.error);
+
+        return t;
+    }
+
+    /**
+     * Calcula el Rango superior de estimación
+     *
+     * @param data
+     * @param proxyE
+     * @return
+     */
+    double calcularUPI(Head data, float proxyE)
+    {
+        double P = this.calcularP(data, proxyE);
+        double range = this.calcularRange(data, proxyE);
+
+        return P + range;
+    }
+
+    /**
+     * Calcula el rango inferior de estimación
+     *
+     * @param data
+     * @param proxyE
+     * @return
+     */
+    double calcularLPI(Head data, float proxyE)
+    {
+        double P = this.calcularP(data, proxyE);
+        double range = this.calcularRange(data, proxyE);
+
+        return P - range;
     }
 }
--- Programa_7_code/src/kernel/ImprimeDatos.java
+++ Programa_7_code/src/kernel/ImprimeDatos.java
@@ -8,7 +8,7 @@ import java.io.File;
  *
  * @author Oscar Vesga
  * @version 1.0
- * @since 01-09-2015
+ * @since 23-11-2015
  *
  */
 public class ImprimeDatos
@@ -30,20 +30,34 @@ public class ImprimeDatos
     public void imprimirDatos(Head data, String test)
     {
         Calculadora calculadora = new Calculadora();
+        
         float proxyE = 386;
-        float B0 = calculadora.calcularB0(data);
-        float B1 = calculadora.calcularB1(data);
+        if (test.equals("test3.txt") || test.equals("test4.txt")){
+            proxyE = 148f;
+        }        
+        
         double r = calculadora.calcularR(data);
         double r2 = calculadora.calcularR2(data);
+        double sig = calculadora.calcularSignificancia(data);
+        float B0 = calculadora.calcularB0(data);
+        float B1 = calculadora.calcularB1(data);
 
         System.out.println("---------" + test + "----------");
-        System.out.println("B0 = " + B0);
-        System.out.println("B1 = " + B1);
         System.out.println("r = " + r);
         System.out.println("r2 = " + r2);
+        System.out.println("Sig = "+ sig);
+        System.out.println("B0 = " + B0);
+        System.out.println("B1 = " + B1);        
 
         float P = calculadora.calcularP(data, proxyE);
+        double range = calculadora.calcularRange(data, proxyE);
+        double UPI = calculadora.calcularUPI(data, proxyE);
+        double LPI = calculadora.calcularLPI(data, proxyE);
+        
         System.out.println("P = " + P);
+        System.out.println("Range = " + range);
+        System.out.println("UPI(70%) = " + UPI);
+        System.out.println("LPI(70%) = " + LPI);
     }
 
     /**
